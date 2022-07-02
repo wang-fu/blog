395db48eb15737ba43150d7b4d2a091f76bef9e8
flyfuwang <flyfuwang@tencent.com>
2022-07-02 13:47:21 +0800

    test ci

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
 git config --global user.email "wangfu@@foxmail.com"
 git config --global user.name "fly"
 git add -A
@@ -23,5 +22,5 @@ echo '文章推送到 github pages中...'
 # git push origin `git subtree split --prefix=docs/.vuepress/dist master`:gh-pages --force
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://{{secrets.test}}@github.com/wang-fu/wang-fu.github.iocc.git master:gh-pages
+git push docs/.vuepress/dist gh-pages
 cd -
