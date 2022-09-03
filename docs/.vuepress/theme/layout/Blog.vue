fff2d6faf6c2b01f81b548735e3c3911314a4606
fly <imwangfu@@foxmail.com>
2022-09-03 18:59:30 +0800

    new

--- docs/.vuepress/theme/layout/Blog.vue
+++ docs/.vuepress/theme/layout/Blog.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="blog">
     <div class="blog__header">
-      <p class="publish-date"><time :datetime="$frontmatter.date">{{ publishDate }}</time></p>
+      <p class="publish-date">
+        <time :datetime="$frontmatter.date">{{ publishDate }}</time>
+      </p>
       <p v-if="$page.readingTime">Time to read: {{ $page.readingTime.text }}</p>
       <h1 class="blog__title">{{ $page.title }}</h1>
     </div>
     <Content class="custom" />
-    <div  class="copyright" style="white-space: break-spaces">
+    <div class="copyright" style="white-space: break-spaces">
+      <pre>
 ——————
 文档信息
 
@@ -16,54 +19,38 @@
 原链接：<a target="__blank" v-bind:href="'https://imwangfu.com' + $page.path" >https://imwangfu.com{{$page.path}}</a>
 版权声明：如需转载，请邮件知会 imwangfu@gmail.com，并保留此文档信息申明
 ——————
-</div>
-  
+    </pre>
+    </div>
+    <div class="wx-qrcode">
+      <img src="/wx-qrcode2.png" alt="" />
+    </div>
 
     <div class="page-edit">
-      <div
-        class="edit-link"
-        v-if="editLink"
-      >
-        <a
-          :href="editLink"
-          target="_blank"
-          rel="noopener noreferrer"
-        >{{ editLinkText }}</a>
-        <OutboundLink/>
+      <div class="edit-link" v-if="editLink">
+        <a :href="editLink" target="_blank" rel="noopener noreferrer">{{
+          editLinkText
+        }}</a>
+        <OutboundLink />
       </div>
-      <div
-        class="last-updated"
-        v-if="lastUpdated"
-      >
+      <div class="last-updated" v-if="lastUpdated">
         <span class="prefix">{{ lastUpdatedText }}: </span>
-        <time class="time" :datetime="$page.lastUpdated">{{ lastUpdated }}</time>
+        <time class="time" :datetime="$page.lastUpdated">{{
+          lastUpdated
+        }}</time>
       </div>
     </div>
 
     <div class="page-nav" v-if="prev || next">
       <p class="inner">
-        <span
-          v-if="prev"
-          class="prev"
-        >
+        <span v-if="prev" class="prev">
           ←
-          <router-link
-            v-if="prev"
-            class="prev"
-            :to="prev.path"
-          >
+          <router-link v-if="prev" class="prev" :to="prev.path">
             {{ prev.title || prev.path }}
           </router-link>
         </span>
 
-        <span
-          v-if="next"
-          class="next"
-        >
-          <router-link
-            v-if="next"
-            :to="next.path"
-          >
+        <span v-if="next" class="next">
+          <router-link v-if="next" :to="next.path">
             {{ next.title || next.path }}
           </router-link>
           →
@@ -71,113 +58,116 @@
       </p>
     </div>
 
-    <slot name="bottom"/>
+    <slot name="bottom" />
     <div id="gitalk-container"></div>
   </div>
 </template>
 
 <script>
-import { resolvePage, normalize, outboundRE, endingSlashRE } from '../util'
-import 'gitalk/dist/gitalk.css'
-import Gitalk from 'gitalk'
+import { resolvePage, normalize, outboundRE, endingSlashRE } from "../util";
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
 
 export default {
-  name: 'Blog',
+  name: "Blog",
 
-  props: ['sidebarItems'],
+  props: ["sidebarItems"],
 
   computed: {
-    lastUpdated () {
+    lastUpdated() {
       if (this.$page.lastUpdated) {
-        const dateFormat = new Date(this.$page.lastUpdated)
+        const dateFormat = new Date(this.$page.lastUpdated);
 
         const options = {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-        } 
-        
-        return `${dateFormat.toLocaleDateString(this.$lang, options)}, ${dateFormat.toLocaleTimeString(this.$lang)}`
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        };
+
+        return `${dateFormat.toLocaleDateString(
+          this.$lang,
+          options
+        )}, ${dateFormat.toLocaleTimeString(this.$lang)}`;
       }
     },
 
-    lastUpdatedText () {
-      if (typeof this.$themeLocaleConfig.lastUpdated === 'string') {
-        return this.$themeLocaleConfig.lastUpdated
+    lastUpdatedText() {
+      if (typeof this.$themeLocaleConfig.lastUpdated === "string") {
+        return this.$themeLocaleConfig.lastUpdated;
       }
-      if (typeof this.$site.themeConfig.lastUpdated === 'string') {
-        return this.$site.themeConfig.lastUpdated
+      if (typeof this.$site.themeConfig.lastUpdated === "string") {
+        return this.$site.themeConfig.lastUpdated;
       }
-      return 'Last Updated'
+      return "Last Updated";
     },
 
-    prev () {
-      const prev = this.$page.frontmatter.prev
+    prev() {
+      const prev = this.$page.frontmatter.prev;
       if (prev === false) {
-        return
+        return;
       } else if (prev) {
-        return resolvePage(this.$site.pages, prev, this.$route.path)
+        return resolvePage(this.$site.pages, prev, this.$route.path);
       } else {
-        return resolvePrev(this.$page, this.sidebarItems)
+        return resolvePrev(this.$page, this.sidebarItems);
       }
     },
 
-    next () {
-      const next = this.$page.frontmatter.next
+    next() {
+      const next = this.$page.frontmatter.next;
       if (next === false) {
-        return
+        return;
       } else if (next) {
-        return resolvePage(this.$site.pages, next, this.$route.path)
+        return resolvePage(this.$site.pages, next, this.$route.path);
       } else {
-        return resolveNext(this.$page, this.sidebarItems)
+        return resolveNext(this.$page, this.sidebarItems);
       }
     },
 
-    editLink () {
+    editLink() {
       if (this.$page.frontmatter.editLink === false) {
-        return
+        return;
       }
       const {
         repo,
         editLinks,
-        docsDir = '',
-        docsBranch = 'master',
-        docsRepo = repo
-      } = this.$site.themeConfig
+        docsDir = "",
+        docsBranch = "master",
+        docsRepo = repo,
+      } = this.$site.themeConfig;
 
-      let path = normalize(this.$page.path)
+      let path = normalize(this.$page.path);
       if (endingSlashRE.test(path)) {
-        path += 'README.md'
+        path += "README.md";
       } else {
-        path += '.md'
+        path += ".md";
       }
       if (docsRepo && editLinks) {
-        return this.createEditLink(repo, docsRepo, docsDir, docsBranch, path)
+        return this.createEditLink(repo, docsRepo, docsDir, docsBranch, path);
       }
     },
 
-    editLinkText () {
+    editLinkText() {
       return (
         this.$themeLocaleConfig.editLinkText ||
         this.$site.themeConfig.editLinkText ||
         `Edit this page`
-      )
+      );
     },
 
     publishDate() {
-        let dateFormat = new Date(this.$frontmatter.date);
-        const removeUtcString = this.$frontmatter.date.split('.000Z');
-        // 默认时区是 utc, 把 .000Z 时区标识去掉
-        if (removeUtcString && removeUtcString.length) {
-          dateFormat = new Date(removeUtcString[0])
-        }
-        const options = {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-        }
-        return dateFormat.toLocaleDateString(this.$lang, options)
-    }
+      let dateFormat = new Date(this.$frontmatter.date);
+      const removeUtcString = this.$frontmatter.date.split(".000Z");
+      // 默认时区是 utc, 把 .000Z 时区标识去掉
+      if (removeUtcString && removeUtcString.length) {
+        dateFormat = new Date(removeUtcString[0]);
+      }
+      const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+      return dateFormat.toLocaleDateString(this.$lang, options);
+    },
   },
   mounted: () => {
     // const gitalk = new Gitalk({
@@ -192,66 +182,69 @@ export default {
     // gitalk.render('gitalk-container');
   },
   methods: {
-    createEditLink (repo, docsRepo, docsDir, docsBranch, path) {
-      const bitbucket = /bitbucket.org/
+    createEditLink(repo, docsRepo, docsDir, docsBranch, path) {
+      const bitbucket = /bitbucket.org/;
       if (bitbucket.test(repo)) {
-        const base = outboundRE.test(docsRepo)
-          ? docsRepo
-          : repo
+        const base = outboundRE.test(docsRepo) ? docsRepo : repo;
         return (
-          base.replace(endingSlashRE, '') +
-           `/${docsBranch}` +
-           (docsDir ? '/' + docsDir.replace(endingSlashRE, '') : '') +
-           path +
-           `?mode=edit&spa=0&at=${docsBranch}&fileviewer=file-view-default`
-        )
+          base.replace(endingSlashRE, "") +
+          `/${docsBranch}` +
+          (docsDir ? "/" + docsDir.replace(endingSlashRE, "") : "") +
+          path +
+          `?mode=edit&spa=0&at=${docsBranch}&fileviewer=file-view-default`
+        );
       }
 
       const base = outboundRE.test(docsRepo)
         ? docsRepo
-        : `https://github.com/${docsRepo}`
+        : `https://github.com/${docsRepo}`;
 
       return (
-        base.replace(endingSlashRE, '') +
+        base.replace(endingSlashRE, "") +
         `/edit/${docsBranch}` +
-        (docsDir ? '/' + docsDir.replace(endingSlashRE, '') : '') +
+        (docsDir ? "/" + docsDir.replace(endingSlashRE, "") : "") +
         path
-      )
-    }
-  }
+      );
+    },
+  },
+};
+
+function resolvePrev(page, items) {
+  return find(page, items, -1);
 }
 
-function resolvePrev (page, items) {
-  return find(page, items, -1)
+function resolveNext(page, items) {
+  return find(page, items, 1);
 }
 
-function resolveNext (page, items) {
-  return find(page, items, 1)
-}
-
-function find (page, items, offset) {
-  const res = []
-  items.forEach(item => {
-    if (item.type === 'group') {
-      res.push(...item.children || [])
+function find(page, items, offset) {
+  const res = [];
+  items.forEach((item) => {
+    if (item.type === "group") {
+      res.push(...(item.children || []));
     } else {
-      res.push(item)
+      res.push(item);
     }
-  })
+  });
   for (let i = 0; i < res.length; i++) {
-    const cur = res[i]
-    if (cur.type === 'page' && cur.path === page.path) {
-      return res[i + offset]
+    const cur = res[i];
+    if (cur.type === "page" && cur.path === page.path) {
+      return res[i + offset];
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-@import '../styles/config.styl'
-@require '../styles/wrapper.styl'
+@import '../styles/config.styl';
+@require '../styles/wrapper.styl';
+
 .blog {
-  @extend $wrapper
+  @extend $wrapper;
+}
+
+.wx-qrcode {
+  padding: 20px 0px 10px 0px;
 }
 
 .blog__header {
@@ -267,54 +260,76 @@ function find (page, items, offset) {
   font-family: 'Poppins';
 }
 
-.page-edit
-  @extend $wrapper
-  padding-top 1rem
-  padding-bottom 1rem
-  padding-left 0
-  padding-right 0
-  overflow auto
-  .edit-link
-    display inline-block
-    a
-      color lighten($textColor, 25%)
-      margin-right 0.25rem
-  .last-updated
-    float right
-    font-size 0.9em
-    .prefix
-      font-weight 500
-      color lighten($textColor, 25%)
-    .time
-      font-weight 400
-      color #aaa
+.page-edit {
+  @extend $wrapper;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  padding-left: 0;
+  padding-right: 0;
+  overflow: auto;
 
-.page-nav
-  padding-top 1rem
-  padding-bottom 0
-  .inner
-    min-height 2rem
-    margin-top 0
-    border-top 1px solid $borderColor
-    padding-top 1rem
-    overflow auto // clear float
-  .next
-    float right
+  .edit-link {
+    display: inline-block;
 
-@media (max-width: $MQMobile)
-  .page-edit
-    .edit-link
-      margin-bottom .5rem
-    .last-updated
-      font-size .8em
-      float none
-      text-align left
+    a {
+      color: lighten($textColor, 25%);
+      margin-right: 0.25rem;
+    }
+  }
+
+  .last-updated {
+    float: right;
+    font-size: 0.9em;
+
+    .prefix {
+      font-weight: 500;
+      color: lighten($textColor, 25%);
+    }
+
+    .time {
+      font-weight: 400;
+      color: #aaa;
+    }
+  }
+}
+
+.page-nav {
+  padding-top: 1rem;
+  padding-bottom: 0;
+
+  .inner {
+    min-height: 2rem;
+    margin-top: 0;
+    border-top: 1px solid $borderColor;
+    padding-top: 1rem;
+    overflow: auto; // clear float
+  }
+
+  .next {
+    float: right;
+  }
+}
+
+@media (max-width: $MQMobile) {
+  .page-edit {
+    .edit-link {
+      margin-bottom: 0.5rem;
+    }
+
+    .last-updated {
+      font-size: 0.8em;
+      float: none;
+      text-align: left;
+    }
+  }
+}
 
 @media (max-width: $MQMobileNarrow) {
   .blog__title {
     font-size: 2.441rem;
   }
 }
+
 .copyright {
   white-space: break-spaces;
   font-size: 14px;
@@ -323,7 +338,7 @@ function find (page, items, offset) {
 </style>
 
 <style>
-.blog img{
+.blog img {
   width: 100%;
 }
 </style>
