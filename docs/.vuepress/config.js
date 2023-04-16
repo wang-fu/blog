72cafab1b5d91e878cf7195f87a5ddf00a6bb724
fly <imwangfu@@foxmail.com>
2023-04-16 17:28:51 +0800

    md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,110 +4,110 @@ module.exports = {
   permalink: "/:year/:month/:slug.html",
   markdown: {
     toc: { includeLevel: [1, 2] },
-    extendMarkdown: md => {
-      console.log(md.renderer.rules)
+    // extendMarkdown: md => {
+    //   console.log(md.renderer.rules)
 
-      const defaultOpenRenderer = (tokens, idx, options, env, self) => self.renderToken(tokens, idx, options);
+    //   const defaultOpenRenderer = (tokens, idx, options, env, self) => self.renderToken(tokens, idx, options);
     
-      const buildTheme = (themeTpl) => {
-        let mapping = {};
-        const base = Object.assign({}, defaultTheme.BASE, {
-          "font-family": ' -apple-system-font, BlinkMacSystemFont, "Helvetica Neue", "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei UI", "Microsoft YaHei", Arial, sans-serif;',
-          "font-size": '14px',
-        })
-        for (let ele in themeTpl.inline) {
-          if (themeTpl.inline.hasOwnProperty(ele)) {
-            let style = themeTpl.inline[ele];
-            mapping[ele] = Object.assign({}, themeTpl.BASE, style);
-          }
-        }
+    //   const buildTheme = (themeTpl) => {
+    //     let mapping = {};
+    //     const base = Object.assign({}, defaultTheme.BASE, {
+    //       "font-family": ' -apple-system-font, BlinkMacSystemFont, "Helvetica Neue", "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei UI", "Microsoft YaHei", Arial, sans-serif;',
+    //       "font-size": '14px',
+    //     })
+    //     for (let ele in themeTpl.inline) {
+    //       if (themeTpl.inline.hasOwnProperty(ele)) {
+    //         let style = themeTpl.inline[ele];
+    //         mapping[ele] = Object.assign({}, themeTpl.BASE, style);
+    //       }
+    //     }
   
-        for (let ele in themeTpl.block) {
-          if (themeTpl.block.hasOwnProperty(ele)) {
-            let style = themeTpl.block[ele];
-            mapping[ele] =  Object.assign({}, base, style);
-          }
-        }
-        return mapping;
-      };
+    //     for (let ele in themeTpl.block) {
+    //       if (themeTpl.block.hasOwnProperty(ele)) {
+    //         let style = themeTpl.block[ele];
+    //         mapping[ele] =  Object.assign({}, base, style);
+    //       }
+    //     }
+    //     return mapping;
+    //   };
 
-      const styleMapping = buildTheme(defaultTheme)
-      let getStyles = (tokenName, addition) => {
-        let arr = [];
-        let dict = styleMapping[tokenName];
-        if (!dict) return "";
-        for (const key in dict) {
-          arr.push(key + ":" + dict[key]);
-        }
-        return `${arr.join(";") + (addition || "")}`;
-      };
+    //   const styleMapping = buildTheme(defaultTheme)
+    //   let getStyles = (tokenName, addition) => {
+    //     let arr = [];
+    //     let dict = styleMapping[tokenName];
+    //     if (!dict) return "";
+    //     for (const key in dict) {
+    //       arr.push(key + ":" + dict[key]);
+    //     }
+    //     return `${arr.join(";") + (addition || "")}`;
+    //   };
    
 
-      // 使用更多的 markdown-it 插件!
-      md.renderer.rules.bullet_list_open = function (tokens, idx, options, env, self) {
-        tokens[idx].attrJoin("class", "bullet_list_open")
-        tokens[idx].attrJoin("style", getStyles('ul'))
-        // console.log(tokens[idx])
-        return defaultOpenRenderer(tokens, idx, options, env, self)
-      };
+    //   // 使用更多的 markdown-it 插件!
+    //   md.renderer.rules.bullet_list_open = function (tokens, idx, options, env, self) {
+    //     tokens[idx].attrJoin("class", "bullet_list_open")
+    //     tokens[idx].attrJoin("style", getStyles('ul'))
+    //     // console.log(tokens[idx])
+    //     return defaultOpenRenderer(tokens, idx, options, env, self)
+    //   };
 
  
 
-      md.renderer.rules.ordered_list_open = function (tokens, idx, options, env, self) {
-        tokens[idx].attrJoin("class", "list_item_open")
-        tokens[idx].attrJoin("style", getStyles('ol'))
-        return defaultOpenRenderer(tokens, idx, options, env, self)
-      };
-      md.renderer.rules.list_item_open = function (tokens, idx, options, env, self) {
-        tokens[idx].attrJoin("class", "ordered_list_close")
-        tokens[idx].attrJoin("style", getStyles('listitem'))
+    //   md.renderer.rules.ordered_list_open = function (tokens, idx, options, env, self) {
+    //     tokens[idx].attrJoin("class", "list_item_open")
+    //     tokens[idx].attrJoin("style", getStyles('ol'))
+    //     return defaultOpenRenderer(tokens, idx, options, env, self)
+    //   };
+    //   md.renderer.rules.list_item_open = function (tokens, idx, options, env, self) {
+    //     tokens[idx].attrJoin("class", "ordered_list_close")
+    //     tokens[idx].attrJoin("style", getStyles('listitem'))
 
-        return defaultOpenRenderer(tokens, idx, options, env, self) + '<span>• </span>'
+    //     return defaultOpenRenderer(tokens, idx, options, env, self) + '<span>• </span>'
       
-      };
-      md.renderer.rules.image = function (tokens, idx, options, env, self) {
-        tokens[idx].attrJoin("class", "image")
-        tokens[idx].attrJoin("style", getStyles('img'))
-        const src = tokens[idx].attrGet('src')
-        const alt = tokens[idx].attrGet('alt')
+    //   };
+    //   md.renderer.rules.image = function (tokens, idx, options, env, self) {
+    //     tokens[idx].attrJoin("class", "image")
+    //     tokens[idx].attrJoin("style", getStyles('img'))
+    //     const src = tokens[idx].attrGet('src')
+    //     const alt = tokens[idx].attrGet('alt')
 
-        let subText = "";
-        if (alt) {
-          subText = `<figcaption ${getStyles(
-            "figcaption"
-          )}>${alt}</figcaption>`;
-        }
-        let figureStyles = getStyles("figure");
-        let imgStyles = getStyles("image");
-        return `<figure style='${figureStyles}'><img style='${imgStyles}' src="${src}"  alt="${alt}"/>${subText}</figure>`;
-      };
-      md.renderer.rules.paragraph_open = function (tokens, idx, options, env, self) {
-        tokens[idx].attrJoin("class", "ppp")
-        tokens[idx].attrJoin("style", getStyles('p'))
-        return defaultOpenRenderer(tokens, idx, options, env, self)
-      };
+    //     let subText = "";
+    //     if (alt) {
+    //       subText = `<figcaption ${getStyles(
+    //         "figcaption"
+    //       )}>${alt}</figcaption>`;
+    //     }
+    //     let figureStyles = getStyles("figure");
+    //     let imgStyles = getStyles("image");
+    //     return `<figure style='${figureStyles}'><img style='${imgStyles}' src="${src}"  alt="${alt}"/>${subText}</figure>`;
+    //   };
+    //   md.renderer.rules.paragraph_open = function (tokens, idx, options, env, self) {
+    //     tokens[idx].attrJoin("class", "ppp")
+    //     tokens[idx].attrJoin("style", getStyles('p'))
+    //     return defaultOpenRenderer(tokens, idx, options, env, self)
+    //   };
    
-      md.renderer.rules.heading_open = function (tokens, idx, options, env, self) {
-        tokens[idx].attrJoin("class", "heading_open")
-        if (tokens[idx].tag === 'h1') {
-          tokens[idx].attrJoin("style", getStyles('h1'))
-        }
-        if (tokens[idx].tag === 'h2') {
-          tokens[idx].attrJoin("style", getStyles('h2'))
-        }
-        if (tokens[idx].tag === 'h3') {
-          tokens[idx].attrJoin("style", getStyles('h3'))
-        }
-        if (tokens[idx].tag === 'h4') {
-          tokens[idx].attrJoin("style", getStyles('h4'))
-        }
-        if (tokens[idx].tag === 'h5') {
-          tokens[idx].attrJoin("style", getStyles('h5'))
-        }
-        return defaultOpenRenderer(tokens, idx, options, env, self)
-      };
-      return md
-    }
+    //   md.renderer.rules.heading_open = function (tokens, idx, options, env, self) {
+    //     tokens[idx].attrJoin("class", "heading_open")
+    //     if (tokens[idx].tag === 'h1') {
+    //       tokens[idx].attrJoin("style", getStyles('h1'))
+    //     }
+    //     if (tokens[idx].tag === 'h2') {
+    //       tokens[idx].attrJoin("style", getStyles('h2'))
+    //     }
+    //     if (tokens[idx].tag === 'h3') {
+    //       tokens[idx].attrJoin("style", getStyles('h3'))
+    //     }
+    //     if (tokens[idx].tag === 'h4') {
+    //       tokens[idx].attrJoin("style", getStyles('h4'))
+    //     }
+    //     if (tokens[idx].tag === 'h5') {
+    //       tokens[idx].attrJoin("style", getStyles('h5'))
+    //     }
+    //     return defaultOpenRenderer(tokens, idx, options, env, self)
+    //   };
+    //   return md
+    // }
   },
   // title: '混沌福王',
   // description: '不会吹口琴的心理咨询师不是好的工程师',
