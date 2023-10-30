c62bab8d14265599b70099e627c1467a155e2f01
Fly <96099979+imwangfu@users.noreply.github.com>
2023-10-30 16:04:57 +0000

    new

--- docs/.vuepress/theme/layout/Blog.vue
+++ docs/.vuepress/theme/layout/Blog.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <div class="blog">
     <div class="blog__header">
       <p class="publish-date">
@@ -63,6 +64,9 @@
 
     <slot name="bottom" />
     <div id="gitalk-container"></div>
+  </div>
+  <div class="blog" id="disqus_thread">
+  </div>
   </div>
 </template>
 
@@ -183,6 +187,24 @@ export default {
     //   distractionFreeMode: false  // Facebook-like distraction free mode
     // })
     // gitalk.render('gitalk-container');
+
+
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+      (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://hun-dun-fu-wang.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+
   },
   methods: {
     createEditLink(repo, docsRepo, docsDir, docsBranch, path) {
